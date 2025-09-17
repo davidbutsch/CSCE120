7879e30869374bbb5e5b095de458881e0d55f31a
David <102764748+davidbutsch@users.noreply.github.com>
2025-09-16 21:59:39 -0500

    implement load_driver_data

--- week4/hw/rank.cpp
+++ week4/hw/rank.cpp
@@ -1,16 +1,18 @@
-#include <iostream>
 #include "rank_functions.h"
+#include <iostream>
 
 int main() {
-    // TODO(student): create and load driver data into a vector of drivers using load_driver_data()
+  // TODO(student): create and load driver data into a vector of drivers using
+  // load_driver_data()
+  std::vector<driver> drivers = load_driver_data();
 
-    // TODO(student): if loading driver data failed,
-    //                1) print "Bad input" to standard output
-    //                2) exit the program by returning 1
+  // TODO(student): if loading driver data failed,
+  //                1) print "Bad input" to standard output
+  //                2) exit the program by returning 1
 
-    // TODO(student): set the rankings of the drivers using set_rankings()
+  // TODO(student): set the rankings of the drivers using set_rankings()
 
-    // TODO(student): print the results using print_results()
+  // TODO(student): print the results using print_results()
 
-    return 0;
+  return 0;
 }
--- week4/hw/rank_functions.cpp
+++ week4/hw/rank_functions.cpp
@@ -1,8 +1,95 @@
-#include <iostream>
+#include "rank_functions.h"
 #include <iomanip>
+#include <iostream>
 #include <string>
 #include <vector>
-#include "rank_functions.h"
+
+// splits input string by any whitespace
+// returns vector of substrings
+inline std::vector<std::string> space_split(const std::string &input) {
+
+  std::vector<std::string> split_strings;
+
+  // return if input is empty
+  if (input.empty())
+    return split_strings;
+
+  size_t cur_pos = 0;
+  size_t last_pos = 0;
+
+  // reassign current position on each iteration
+  // push each new substring found between whitespace
+  while ((cur_pos = (input + " ").find(" ", last_pos)) != std::string::npos) {
+
+    std::string split_string = input.substr(last_pos, cur_pos - last_pos);
+
+    // push non-empty split string to vector
+    // empty split strings indicate multiple " " characters next to one another
+    if (!split_string.empty())
+      split_strings.push_back(split_string);
+
+    // reassign last position + single space length
+    last_pos = cur_pos + 1;
+  }
+
+  return split_strings;
+};
+
+// covert string to unsigned int
+inline unsigned int stoui(const std::string &input) {
+  // convert to long
+  long unsigned_long = stoul(input);
+  unsigned int unsigned_int = unsigned_long;
+
+  // check for int overflow
+  if (unsigned_int != unsigned_long)
+    throw std::out_of_range("Value out of range for unsigned int");
+
+  return unsigned_int;
+}
+
+inline bool is_uppercase_letter(char c) { return (c >= 'A' && c <= 'Z'); }
+inline bool is_letter(char c) {
+  return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+}
+
+inline void validate_driver(const driver &driver) {
+  // check for positive race time
+  if (driver.time < 0)
+    throw std::invalid_argument("Driver time must be positive.");
+
+  // check for valid country code
+  if (driver.country.length() != 3)
+    throw std::invalid_argument(
+        "Driver country code must be 3 characters in length.");
+  // check for valid characters (uppercase letters only)
+  else
+    for (char c : driver.country)
+      if (!is_uppercase_letter(c))
+        throw std::invalid_argument(
+            "Driver country code must only contain uppercase letters.");
+
+  // check range of car number ([0, 99])
+  // no need to check if less than 0 because it is unsigned (already validated
+  // during conversion)
+  if (driver.number > 99)
+    throw std::invalid_argument("Driver car number must be in range [0, 99].");
+
+  // check for valid last name (letters or spaces only, at least 2 letters)
+  int letter_count = 0;
+  for (char c : driver.lastname) {
+    // count letters
+    if (is_letter(c))
+      letter_count++;
+    // validate char type
+    else if (c != ' ')
+      throw std::invalid_argument(
+          "Driver last name must only contain letters or spaces.");
+  }
+  if (letter_count < 2)
+    throw std::invalid_argument(
+        "Driver last name must contain at least 2 letters.");
+}
 
 // load data from standard input into a vector
 //   input format := <time> <country> <number> <name>
@@ -12,43 +99,81 @@
 //   rank should be initialized to 0 for each driver
 // returns a vector of drivers, or an empty vector if any input is invalid
 std::vector<driver> load_driver_data() {
-    // TODO(student)
-    return {};
+
+  std::string current_line;
+  std::vector<std::string> lines;
+
+  while (std::getline(std::cin, current_line))
+    // if next line is empty break out of loop
+    if (current_line.empty())
+      break;
+    // else push non-empty lines into lines vector
+    else
+      lines.push_back(current_line);
+
+  std::vector<driver> drivers;
+
+  // parse lines, validate driver info, and push structs to drivers vector
+  for (std::string line : lines) {
+    std::vector<std::string> driver_props = space_split(line);
+
+    driver cur_driver = {
+        driver_props.at(3),        // lastname
+        driver_props.at(1),        // country
+        stoui(driver_props.at(2)), // number
+        stod(driver_props.at(0)),  // time
+        0                          // rank
+    };
+
+    // validate driver (throws invalid_argument)
+    validate_driver(cur_driver);
+
+    drivers.push_back(cur_driver);
+  }
+
+  return drivers;
 }
 
-// returns a copy of the input vector with ranks set based on the time for each driver.
+// returns a copy of the input vector with ranks set based on the time for each
+// driver.
 //   the fastest/minimum time is ranked 1
 // the order of the elements in the vector should not be changed
 std::vector<driver> set_rankings(std::vector<driver>) {
-    // TODO(student)
-    return {};
+  // TODO(student)
+  return {};
 }
 
-// returns a copy of the input string with whitespace removed from the front and back
 std::string trim(std::string) {
-    // TODO(student)
-    return "";
+  // TODO(student)
+  return "";
 }
 
 // print the results of the race
-void print_results(const std::vector<driver>& drivers) {
-    // get the fastest time
-    double best_time;
-    for (const driver& driver : drivers) {
-        if (driver.rank == 1) {
-            best_time = driver.time;
-            break;
-        }
+void print_results(const std::vector<driver> &drivers) {
+  // get the fastest time
+  double best_time;
+  for (const driver &driver : drivers) {
+    if (driver.rank == 1) {
+      best_time = driver.time;
+      break;
     }
+  }
 
-    std::cout << "Final results!";
-    std::cout << std::setprecision(2) << std::showpoint << std::fixed << std::endl;
-    for (unsigned rank = 1; rank <= drivers.size(); rank++) {
-        for (const driver& driver : drivers) {
-            if (driver.rank == rank) {
-                std::string rank_str = "["+std::to_string(rank)+"]";
-                std::cout << std::setw(4) << std::left << rank_str << " " << driver.time << " " << std::setw(15) << std::left << driver.lastname << " (" << driver.country << ") +" << (driver.time - best_time) << std::endl;
-            }
-        }
+  std::cout << "Final results!";
+  std::cout << std::setprecision(2) << std::showpoint << std::fixed
+            << std::endl;
+  for (unsigned rank = 1; rank <= drivers.size(); rank++) {
+    for (const driver &driver : drivers) {
+      if (driver.rank == rank) {
+        std::string rank_str = "[" + std::to_string(rank) + "]";
+        std::cout
+            << std::setw(4)
+            // returns a copy of the input string with whitespace removed from
+            // the front and back<< std::left << rank_str << " " << driver.time
+            << " " << std::setw(15) << std::left << driver.lastname << " ("
+            << driver.country << ") +" << (driver.time - best_time)
+            << std::endl;
+      }
     }
+  }
 }
